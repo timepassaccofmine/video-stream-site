f152f73a9994170eab17cb1b6882ab3642d6bf3d
Sarvesh Jangam <timepassaccofmine@gmail.com>
2025-06-28 01:26:01 +0530

    utils updated

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import express from "express";
 import 'dotenv/config';
 import { connectDB } from "./db/index.js";
 
-const app=express();
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server listening on Port: ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+    console.log(`Mongodb connection error!!! `,err);
+});
---
+++ src/utils/AsyncHandler.js
@@ -0,0 +1,21 @@
+const AsyncHandler =(requestHandler)=>{
+    return (req,res,next)=>{
+        Promise
+        .resolve((req,res,next))
+        .catch((err)=>next(err))
+    }
+}
+
+export {AsyncHandler}
+
+// const asyncHandle=(fn)=>async (req,res,next)=>{
+//     try{
+//         await fn(req,res,next);
+//     }
+//     catch(error){
+//         res.status(error.code || 500).json({
+//             success:false,
+//             message:err.message
+//         })
+//     }
+// }
