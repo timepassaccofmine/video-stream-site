dd6baa7a212511eb8b70ad6b13bb30f0339487f3
Sarvesh Jangam <timepassaccofmine@gmail.com>
2025-06-29 20:29:09 +0530

    Finally configuration or setup for a Proffessional grade backend project done

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,42 @@
+import { v2 as cloudinary } from 'cloudinary';
+import fs from "fs"
+
+// Configuration
+cloudinary.config({ 
+    cloud_name: `${process.env.CLOUDINARY_CLOUD_NAME}`, 
+    api_key: `${process.env.CLOUDINARY_API_KEY}`, 
+    api_secret: `${CLOUDINARY_API_SECCRET}` // Click 'View API Keys' above to copy your API secret
+});
+
+const uploadOnCloudinary=async (localFilePath)=>{
+    try{
+        if(!localFilePath) return null;
+        //upload on cloudinary
+        const response=await cloudinary.uploader.upload(`${localFilePath}`, 
+            {
+                resource_type:"auto"
+            }
+        )
+        //file has been uploaded successfully
+        console.log("file is uploaded on cloudinary and response url is: ",response.url)
+        return response.url
+    }
+    catch(error){
+        console.log(`unlinking ${localFilePath}!!`)
+        fs.unlinkSync(localFilePath) //remove the locally saved temp file as the upload failed
+        return null;
+    }
+}
+
+// Upload an image
+const uploadResult = await cloudinary.uploader
+.upload(
+    'https://res.cloudinary.com/demo/image/upload/getting-started/shoes.jpg', {
+        public_id: 'shoes',
+    }
+)
+.catch((error) => {
+    console.log(error);
+});
+    
+    
